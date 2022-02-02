03725cebe35ac9dc95ede45e82e51c88604516f1
semkor <selsasss@gmail.com>
2022-02-02 10:00:29 +0200

    new22

---
+++ src/main/java/lesson3/hw1/Demo.java
@@ -0,0 +1,19 @@
+package lesson3.hw1;
+
+public class Demo {
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+
+//поиск продуктов по диапазону параметров
+//        System.out.println(solution.findProductsByPrice(100, 10));
+
+
+//поиск по имени
+        System.out.println(solution.findProductsByName("toy"));
+
+
+//поиск продкутов с пустым описанием
+//        System.out.println(solution.findProductsWithEmptyDescription());
+
+    }
+}
---
+++ src/main/java/lesson3/hw1/Solution.java
@@ -0,0 +1,99 @@
+package lesson3.hw1;
+
+import java.sql.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+    private static final String DB_URL = "jdbc:mysql://database-1.c0nqcnerv9wp.us-east-1.rds.amazonaws.com:3306/database_name";
+    private static final String USER = "maven";
+    private static final String PASS = "1291328diMA";
+
+//a. поиск  продуктов с заданной ценой в диапазоне +-delta включительно (при 100 будет искать от 90 до 110)
+    public List<Product> findProductsByPrice(int price, int delta) {
+        try (Connection connection = getConnection();
+             PreparedStatement prepareStatement = connection.prepareStatement("SELECT * FROM PRODUCTM WHERE PRICE BETWEEN ? AND ?")) {
+            prepareStatement.setInt(1, (price - delta));
+            prepareStatement.setInt(2, (price + delta));
+
+            List<Product> products = new ArrayList<>();
+            ResultSet resultSet = prepareStatement.executeQuery();
+            while (resultSet.next()) {
+                Product product = new Product(resultSet.getLong(1), resultSet.getString(2), resultSet.getString(3), resultSet.getInt(4));
+                products.add(product);
+            }
+            return products;
+        } catch (SQLException e) {
+            System.out.println("Something  went wrong");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+/*b. продукты, которые содержат в своем имени слово word*
+            * - если слово word является неккоректным (больше одного слова в стринге, длина меньше 3, содержит спецсимволы)
+                   - тогда выбрасывает ошибку, которая в описании обязательно должна содержать само слово и описание ошибки*/
+    public List<Product> findProductsByName(String word) {
+        try (Connection connection = getConnection();
+             PreparedStatement prepareStatement = connection.prepareStatement("SELECT * FROM PRODUCTM WHERE NAME  LIKE ? ")) {
+            prepareStatement.setString(1, ("%" + word + "%"));
+
+            ResultSet resultSet = prepareStatement.executeQuery();
+
+            List<Product> products = new ArrayList<>();
+
+            while (resultSet.next()) {
+                String validation=resultSet.getString(2);
+//                if (validation.length() < 3)
+//                    throw new Exception("User with id: " + validation + " already exist");
+//              else if (!validation.matches("[a-z]+"))yfg
+//                  throw new Exception("User with id: " + validation + " already exist");
+
+//                String str="%" + word + "%";  /^123abc$/  ^*toy*{2,}$
+                if(validation.matches("toy"))
+                    System.out.println(validation);
+
+
+
+
+
+                Product product = new Product(resultSet.getLong(1), resultSet.getString(2), resultSet.getString(3), resultSet.getInt(4));
+                products.add(product);
+            }
+            return products;
+        } catch (SQLException e) {
+            System.out.println("Something  went wrong");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+//с. продукты с пустым полем описания
+    public List<Product> findProductsWithEmptyDescription() {
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery("SELECT *, LENGTH (DESCRIPTON) FROM PRODUCTM WHERE LENGTH (DESCRIPTON)=0");
+            List<Product> products = new ArrayList<>();
+
+            while (resultSet.next()) {
+                Product product = new Product(resultSet.getLong(1), resultSet.getString(2), resultSet.getString(3), resultSet.getInt(4));
+                products.add(product);
+            }
+            return products;
+        } catch (SQLException e) {
+            System.out.println("Something  went wrong");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+//----------------------------------------------------------------------------------------------------------------------
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USER, PASS);
+    }
+}
